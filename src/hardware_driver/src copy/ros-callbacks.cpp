aab27ffd57684d744c6ce1283f7f97ea384db7e2
Nick Steele <nichlock@gmail.com>
2023-02-22 17:21:35 -0500

    Added include directory, removed old hardware_driver code

--- src/hardware_driver/src copy/ros-callbacks.cpp
+++
@@ -1,8 +0,0 @@
-#include "ros-callbacks.hpp"
-#include "hardware-driver-main.hpp"
-
-#include <ros/ros.h>
-
-void motionMotorSubCallback(const custom_msgs::MotorControls::ConstPtr& cmd) {
-  ros_data::motor_controls_msg = (*cmd);
-}
--- src/hardware_driver/src copy/ros-callbacks.hpp
+++
@@ -1,3 +0,0 @@
-#include "hardware-driver-main.hpp"
-
-void motionMotorSubCallback(const custom_msgs::MotorControls::ConstPtr& cmd);
